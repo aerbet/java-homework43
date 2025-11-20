bfdfdf0313a9e177d6653f37f72f9920c395b9c6
Erbol Zhaparov <ae.270718@gmail.com>
2025-11-20 23:05:11 +0600

    add root path, add handlePage method to send html response

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ import java.nio.charset.Charset;
 import server.BaseServer;
 import java.net.URI;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
+    private static final Path ROOT = Paths.get("src/data");
+
     public static void main(String[] args) {
         try {
             HttpServer server = BaseServer.makeServer();
@@ -19,9 +24,42 @@ public class Main {
     }
 
     private static void initRoutes(HttpServer server) {
+        server.createContext("/index.html", Main::handlePage);
         server.createContext("/", Main::handleRequest);
         server.createContext("/apps/", Main::handleRequestApps);
         server.createContext("/apps/profile", Main::handleRequestProfile);
+    }
+
+    private static void handlePage(HttpExchange exchange) {
+        byte[] data;
+        try {
+            data = Files.readAllBytes(ROOT.resolve("index.html"));
+        } catch (IOException ioe) {
+            try {
+                String errorMsg = "404 Not Found" + ROOT.resolve("index.html");
+                exchange.sendResponseHeaders(404, errorMsg.getBytes(StandardCharsets.UTF_8).length);
+                exchange.getResponseBody().write(errorMsg.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException innerIoe) {
+                innerIoe.printStackTrace();
+            }
+            System.err.println("Error reading file: " + ROOT.resolve("index.html"));
+            ioe.printStackTrace();
+            return;
+        }
+
+        try {
+            exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+            int responseCode = 200;
+            int length = data.length;
+
+            exchange.sendResponseHeaders(responseCode, length);
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(data);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 
     private static void handleRequestProfile(HttpExchange exchange) {
@@ -32,10 +70,6 @@ public class Main {
             exchange.sendResponseHeaders(responseCode, length);
 
             try (PrintWriter writer = getWriterFrom(exchange)) {
-                String method = exchange.getRequestMethod();
-                URI uri = exchange.getRequestURI();
-                String ctxPath = exchange.getHttpContext().getPath();
-
                 write(writer, "Pentagon", "President DNA is confirmed!");
                 write(writer, "Pentagon", "Welcome Mr. President!");
                 writer.flush();
@@ -53,10 +87,6 @@ public class Main {
             exchange.sendResponseHeaders(responseCode, length);
 
             try (PrintWriter writer = getWriterFrom(exchange)) {
-                String method = exchange.getRequestMethod();
-                URI uri = exchange.getRequestURI();
-                String ctxPath = exchange.getHttpContext().getPath();
-
                 write(writer, "Play market", "Your application is suspected of having virus!");
                 write(writer, "Play market", "Fix your issue or we'll delete it from our market!");
                 writer.flush();
